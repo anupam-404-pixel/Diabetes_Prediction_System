9d0e09f79fbcb3e0d7d891fd94d55c7124a1bb8f
anupam-404-pixel <majianupam924@gmail.com>
2026-02-07 00:13:29 +0530

    Update asgi.py

--- DiabetesPrediction/DiabetesPrediction/asgi.py
+++ DiabetesPrediction/DiabetesPrediction/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DiabetesPrediction.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'DiabetesPrediction.DiabetesPrediction.settings'
+)
 
 application = get_asgi_application()
