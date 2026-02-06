502b9621a740463da86b07277b90007d616e5d2a
Anupam Maji <245649742+anupam-404-pixel@users.noreply.github.com>
2026-02-06 19:10:05 +0530

    Changed in allowed host

--- DiabetesPrediction/DiabetesPrediction/settings.py
+++ DiabetesPrediction/DiabetesPrediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vl&vpnu6y^=7%x)2fr$akot0g2mxxq7))s4b6@d@_fo5*s#6@d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
